f366a92f57c786b3a4d616e5a8188075f9e5e38d
Abram Bradley <abram@Abrams-MBP.attlocal.net>
2020-02-08 15:37:16 -0500

    commits

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,6 +1,6 @@
 const redirect = () => {
   if (typeof window === undefined) return
-  window.location.replace(window.location.origin + '/admin')
+  window.location.href = window.location.origin + '/admin/'
 }
 
 export default () => {
