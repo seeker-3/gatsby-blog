9bc2e96cce722e5a1d8dffa94797e2439a76971a
Abram Bradley <abram@Abrams-MBP.attlocal.net>
2020-02-19 11:30:04 -0500

    commits

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
 const { resolve } = require('path')
 
-exports.createPages = ({ actions: { createPage }, graphql }) => {
+exports.createPages = async ({ actions: { createPage }, graphql }) => {
   const component = resolve('src/templates/post.jsx')
 
-  return graphql(`
+  const { errors, data } = await graphql(`
     {
       allMarkdownRemark {
         edges {
@@ -18,22 +18,60 @@ exports.createPages = ({ actions: { createPage }, graphql }) => {
         }
       }
     }
-  `).then(({ errors, data }) => {
-    if (errors) {
-      errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(errors)
-    }
+  `)
 
-    const posts = data.allMarkdownRemark.edges
+  if (errors) {
+    errors.forEach(e => console.error(e.toString()))
+    return Promise.reject(errors)
+  }
 
-    posts.forEach(({ node: { id } }) => {
-      createPage({
-        path: `/posts/${id}`,
-        component,
-        context: {
-          id,
-        },
-      })
+  const posts = data.allMarkdownRemark.edges
+
+  posts.forEach(({ node: { id } }) => {
+    createPage({
+      path: `/posts/${id}`,
+      component,
+      context: {
+        id,
+      },
     })
   })
 }
+
+// exports.createPages = ({ actions: { createPage }, graphql }) => {
+//   const component = resolve('src/templates/post.jsx')
+
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             html
+//             id
+//             frontmatter {
+//               title
+//               date
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(({ errors, data }) => {
+//     if (errors) {
+//       errors.forEach(e => console.error(e.toString()))
+//       return Promise.reject(errors)
+//     }
+
+//     const posts = data.allMarkdownRemark.edges
+
+//     posts.forEach(({ node: { id } }) => {
+//       createPage({
+//         path: `/posts/${id}`,
+//         component,
+//         context: {
+//           id,
+//         },
+//       })
+//     })
+//   })
+// }
